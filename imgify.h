5a2a8ab932699b5cb3156bd6533ae9a04bab0176
Valerij Giletin <somesugar4shell@gmail.com>
2026-06-28 01:37:09 +0300

    renew

--- imgify.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-bool png_load(const char *filename, uint8_t **out_buffer, size_t *out_buffer_size, uint32_t *out_width, uint32_t *out_height, uint8_t *out_channels, uint32_t *out_padding);
-bool png_save(const char *filename, uint8_t *data, uint32_t width, uint32_t height, uint8_t channels, uint32_t padding, uint8_t pad_byte, size_t original_size);
